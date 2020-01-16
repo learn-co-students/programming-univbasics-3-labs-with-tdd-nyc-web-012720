c11a01d968dda65f97daecb504ab9b28babc9fd1
benevolent-trace-0172 <davidmlee4@gmail.com>
2020-01-16 04:39:05 +0000

    Done.

--- calculator.rb
+++ calculator.rb
@@ -1,1 +1,19 @@
 # Add your variables here
+first number = 6
+second_number = 3
+
+def sum(first_number=6, second_number=3)
+  puts first_number + second_number
+end
+
+def difference(first_number=6, second_number=3)
+  puts first_number - second_number
+end
+
+def product(first_number=6, second_number=3)
+  puts first_number*second_number
+end
+
+def quotient(first_number=6, second_number=3)
+  puts first_number/second_number
+end
