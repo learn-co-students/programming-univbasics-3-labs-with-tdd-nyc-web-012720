4c3c35cdcf90d13cebe807c3a21c7aac0b6cd894
benevolent-trace-0172 <davidmlee4@gmail.com>
2020-01-16 04:40:42 +0000

    Done.

--- calculator.rb
+++ calculator.rb
@@ -2,18 +2,18 @@
 first number = 6
 second_number = 3
 
-def sum(first_number=6, second_number=3)
+def sum(first_number, second_number)
   puts first_number + second_number
 end
 
-def difference(first_number=6, second_number=3)
+def difference(first_number, second_number)
   puts first_number - second_number
 end
 
-def product(first_number=6, second_number=3)
+def product(first_number, second_number)
   puts first_number*second_number
 end
 
-def quotient(first_number=6, second_number=3)
+def quotient(first_number, second_number)
   puts first_number/second_number
 end
